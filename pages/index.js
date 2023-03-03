1ceb5aaa36c8e33ae992f6f40b446ffd754f15ac
Abuzar <57677552+abuzariftikhar@users.noreply.github.com>
2023-03-04 00:44:37 +0500

    3d

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ import Link from "next/link";
 
 export default function IndexPage() {
   const redirecttoNativeApp = (/*potential params */) => {
-    document.location = "dlfirebase://";
+    document.location = "applink://";
   };
   return (
     <div>
